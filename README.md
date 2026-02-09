c26a221defe2f2da8da85645a630e6235719ea45
emadahmed97 <emad1997@gmail.com>
2026-02-09 07:10:30 +0000

    fix reqs pt 2

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ emoji: 🕌
 colorFrom: blue
 colorTo: green
 sdk: gradio
-sdk_version: 5.1.0
+sdk_version: 5.27.0
 python_version: "3.10"
 app_file: app.py
 pinned: false
--- app.py
+++ app.py
@@ -568,5 +568,6 @@ if __name__ == "__main__":
         server_port=7860,
         share=False,
         show_api=True,
-        show_error=True
+        show_error=True,
+        ssr_mode=False
     )
